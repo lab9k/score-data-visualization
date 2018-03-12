54e6e23ecd31632c954e81ac31fc798859f58801
Ruben Alliet <allietruben@gmail.com>
2018-03-12 21:35:29 +0100

    compress array to count doubles in keywords

--- js/statistics.js
+++ js/statistics.js
@@ -12,7 +12,6 @@ function getJSONChartData() {
         );
 }
 
-
 /*
     var keywords = {
         "gent": {
@@ -43,26 +42,20 @@ function transformData(JSONdata) {
     cities.forEach(city => {
         JSONdata.forEach(challenge => {
             if(challenge['gsx$city']['$t'] === city) {
-                //console.log(city+ ": " + challenge['gsx$keywords']['$t']); 
+                //"keyword1,keyword2" => [keyword1,keyword2]
                 let challengeKeywords = challenge['gsx$keywords']['$t'].split(", ");
-                //console.log(city + ": " + challengeKeywords);
+                //[keyword1,keyword2] + [keyword1,keyword2] => city: [keyword,keyword,keyword...]
                 cityKeywords[city] = cityKeywords[city].concat(challengeKeywords);
-                //console.log(cityKeywords[city].concat(challengeKeywords));
-                //.split, in array steken,
-
-                // gent: [keyword,keyword,keyword]
             }
         })
-    })
+    });
 
-    for(var key in cityKeywords) {
-        var value = cityKeywords[key];
-        console.log(key + ":\n " + value);
-        // do something with "key" and "value" variables
-       
-      }
-    //console.log("blasdf")
-    return null;
+    cities.forEach(city => {
+        
+    })  
+
+    //count doubles + build up JSON object
+    console.log(compressArray(cityKeywords['Gent']));
 }
     
 
@@ -73,6 +66,53 @@ function initializeMap(pieChartData) {
 
 }
 
+
+function compressArray(original) {
+
+	var compressed = [];
+	// make a copy of the input array
+	var copy = original.slice(0);
+ 
+	// first loop goes over every element
+	for (var i = 0; i < original.length; i++) {
+ 
+		var myCount = 0;	
+		// loop over every element in the copy and see if it's the same
+		for (var w = 0; w < copy.length; w++) {
+			if (original[i] == copy[w]) {
+				// increase amount of times duplicate is found
+				myCount++;
+				// sets item to undefined
+				delete copy[w];
+			}
+		}
+ 
+		if (myCount > 0) {
+			var a = new Object();
+			a.value = original[i];
+			a.count = myCount;
+			compressed.push(a);
+		}
+	}
+ 
+	return compressed;
+};
+
+// It should go something like this:
+
+var testArray = new Array("dog", "dog", "cat", "buffalo", "wolf", "cat", "tiger", "cat");
+var newArray = compressArray(testArray);
+
+ 
+/*
+console: [
+	Object { value="dog", count=2}, 
+	Object { value="cat", count=3}, 
+	Object { value="buffalo", count=1}, 
+	Object { value="wolf", count=1}, 
+	Object { value="tiger", count=1}
+]
+*/
 /*
     var keywords = {
         "gent": {
